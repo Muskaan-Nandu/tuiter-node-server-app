bcbd0dc280ca53e58cc94cb34e72221746edbafd
Muskaan Nandu <nandumuskaan@gmail.com>
2023-08-06 19:14:11 -0400

    correction

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -13,7 +13,16 @@ const createTuit = (req, res) => {
   const newTuit = req.body;
   newTuit._id = (new Date()).getTime() + '';
   newTuit.likes = 0;
+  newTuit.dislikes = 0;
+  newTuit.disliked = false;
   newTuit.liked = false;
+  newTuit.replies = 0;
+  newTuit.username = "NASA";
+  newTuit.handle = "@nasa";
+  newTuit.image = "/images/nasa.png";
+  newTuit.topic = "Space";
+  newTuit.time = "2h";
+  newTuit.retuits = 0;
   tuits.push(newTuit);
   res.json(newTuit);
 }
